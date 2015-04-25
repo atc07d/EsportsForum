d1dec4df488f600a547df4eb43bfe4b240aa2090
atc07d <atc07d@gmail.com>
2015-04-25 11:30:24 -0400

    Email attempt 1

--- signIn.php
+++ signIn.php
@@ -3,7 +3,8 @@
 	// If unique, add to user table
 
 	session_start();
-	error_reporting(0);
+	error_reporting(-1);
+	ini_set('display_errors', 'On');
 	include_once "connect.php";
 
 	// Create connection
@@ -34,6 +35,57 @@
 	//$result3 = $conn->query($sql3);
 
 	
+	// Send mail function
+	// Parameters: users email address and user name
+	// Result: send email to provided address containing validation link
+	// 			validation link returns user to site and activate validation
+	//			user can now do more than just look
+	function sendMail($users_addr, $users_name)
+	{
+		$to = $users_addr;
+
+		$subject = 'CS418 eSports WebSite Validation';
+
+		$message = '
+		<html>
+		<head>
+			<title>Validation Email</title>
+		</head>
+		<body>
+			<h3>Greetings, ' . $users_addr . '!</h3>
+			<p>In order to fully use our website, please conform your existence
+				as a non-robot and validate via the link below</p>
+			<br>
+			<p>link here</p>
+		</body>
+		</html>
+		';
+
+		// To send HTML mail, the Content-type header must be set
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+		// Additional headers
+		$headers .= 'To:'. $users_name .  ' <' . $users_addr . '>' . "\r\n";
+		$headers .= 'From: CS418 eSports WebSite Validation <acoffman@cs.odu.edu>' . "\r\n";
+		$headers .= 'Reply-To: acoffman@cs.odu.edu' . "\r\n";
+		
+
+
+		// Send mail
+		if (mail($to, $subject, $message, $headers))
+		{
+			echo '<br>Please check your email and follow the validation link. Thank you for registering!';
+		}
+
+		else
+		{
+			echo '<br>Mail failed';
+		}
+
+	}
+
+
 	// Flag variable tests for uniqueness in user names in table users from messageboard DB
 	// BUG: Breaks on null value for username. FIX: use isset on on those POST values, both uname and pw? 
 	$flag = 0;
@@ -77,13 +129,16 @@
 			 VALUES ('$UserID','$_POST[username]','$_POST[password]','$_POST[email]')";
 		if ($conn->query($sql2) === TRUE) 
 		{
+			
+			sendMail($_POST['email'], $_POST['username']);
+
 			echo '<p>New record created successfully</p>
 					<form action=index.php>
 						<input type="submit" value="Home">
 					</form>
 				';
 			
-
+				
 			
 
 		} 
