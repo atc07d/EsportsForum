0a116eef7f213589addf5616e2cb47fe62aa668d
Adam Coffman <atc07d@gmail.com>
2015-04-27 16:03:55 -0400

    Update emailValidation.php

--- emailValidation.php
+++ emailValidation.php
@@ -45,7 +45,8 @@
 
      <title>reCAPTCHA demo: Simple page</title>
 
-     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+     <!--<script src="https://www.google.com/recaptcha/api.js" async defer></script>-->
+     <script src='https://www.google.com/recaptcha/api.js'></script>
 
   </head>
   <body>
@@ -53,10 +54,10 @@
     <div class="col-md-3 col-md-offset-2">
     <h3>Please complete the validation process:</h3>
     <form action="/" method="POST">
-      <div class="g-recaptcha" data-theme="dark" data-sitekey="6LfK5wUTAAAAAN7CE-vB3ddApfbi3Fc--ptvF-r4"></div>
+      <!--<div class="g-recaptcha" data-theme="dark" data-sitekey="6LfK5wUTAAAAAN7CE-vB3ddApfbi3Fc--ptvF-r4"></div>-->
       <br>
       <input type="submit" name="submit" value="Submit">
-
+      <div class="g-recaptcha" data-theme="dark" data-sitekey="6LfK5wUTAAAAAN7CE-vB3ddApfbi3Fc--ptvF-r4"></div>
     </form>
     </div>
     </div>
