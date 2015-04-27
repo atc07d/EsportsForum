584859424eb95f2d45367b107100e7685bc203ba
atc07d <atc07d@gmail.com>
2015-04-27 00:58:35 -0400

    GitHub auth example test

--- logIn.php
+++ logIn.php
@@ -61,10 +61,12 @@
 		
 			';
 	}
+
 	if($_POST["github-submit"]) 
 	{
 
 		// Rescources: http://www.binarytides.com/php-add-login-with-github-to-your-website/
+		//				http://www.phpgang.com/how-to-add-github-oauth-login-on-your-website-using-php_740.html
 		// My application info
 		$client_id = '29fbbf34ee6862f70fa3';
 		$clientSecret = '5d20cfd3d5450ce970609674a1f5731835437222';
@@ -73,32 +75,74 @@
 	    //login request
 	    if($_SERVER['REQUEST_METHOD'] == 'POST')
 	    {
-	        $url = "https://github.com/login/oauth/authorize?client_id=$client_id&redirect_uri=$redirect_url&scope=user";
-	        header("Location: $url");
+	        
+		
+			$url = "https://github.com/login/oauth/authorize?client_id=$client_id&redirect_uri=$redirect_url&scope=user";
+	    	header("Location: $url");
+			
+			
+			/*
+			if(setcookie("github",$accessToken))
+			{
+				echo "sucessfully authenticated with github!";
+				header("Location:index.php");
+			}
+			*/
+	    }	    
+	}
 
-	         // Code from course website:https://raw.githubusercontent.com/machawk1/ODUCS418/spring2015/docker_cs418/deployUI.php
-	         $ch = curl_init();
+	if(isset($_GET['code']))
+	{
+     	// Code from course website:https://raw.githubusercontent.com/machawk1/ODUCS418/spring2015/docker_cs418/deployUI.php
+        $ch = curl_init();
 
-			curl_setopt($ch, CURLOPT_URL,"https://github.com/login/oauth/access_token");
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS,
-						http_build_query(array(
-							'code' => $_GET['code'],
-							'client_id' => $clientId,
-							'client_secret' => $clientSecret
+		curl_setopt($ch, CURLOPT_URL,"https://github.com/login/oauth/access_token");
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS,
+					http_build_query(array(
+						'code' => $_GET['code'],
+						'client_id' => $clientId,
+						'client_secret' => $clientSecret
 
-						))
-					);
-			curl_setopt($ch, CURLOPT_HTTPHEADER,array("Accept: application/json"));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			$server_output = curl_exec ($ch);
-			curl_close ($ch);
-			$json = json_decode($server_output,true);
-			$accessToken = json_decode($server_output,true)["access_token"];
-
-	    }	
-
-	    
-
+					))
+				);
+		curl_setopt($ch, CURLOPT_HTTPHEADER,array("Accept: application/json"));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$server_output = curl_exec ($ch);
+		curl_close ($ch);
+		$json = json_decode($server_output,true);
+		//$accessToken = json_decode($server_output,true)["access_token"];
+		
+		if ($json->access_token)
+		{
+			$data = array('url' => 'https://api.github.com/user?access_token='.$json->access_token,
+                  'header' => array("Content-Type: application/x-www-form-urlencoded","User-Agent: CS418M4","Accept: application/json"),
+                  'method' => 'GET');
+    
+	        $gitUser = json_decode(curlRequest($data));
+	        
+	        echo '
+	        <table>
+	            <tr>
+	                <td colspan="2"><a href="'.$gitUser->html_url.'" target="_blank"><img src="'.$gitUser->avatar_url.'" width="200px" height="200px"/></a></td>
+	            </tr>
+	            <tr>
+	                <td>Name:</td>
+	                <td>'.$gitUser->name.'</td>
+	            </tr>
+	            <tr>
+	                <td>Email:</td>
+	                <td>'.$gitUser->email.'</td>
+	            </tr>
+	            <tr>
+	                <td>Location:</td>
+	                <td>'.$gitUser->location.'</td>
+	            </tr>
+	            <tr>
+	                <td>Website:</td>
+	                <td>'.$gitUser->blog.'</td>
+	            </tr>
+	        </table>';
+		}
 	}
 ?>
