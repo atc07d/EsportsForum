a2a780c49b41ea796f16a7f202a5ac54ea47ac5b
atc07d <atc07d@gmail.com>
2015-04-27 10:21:30 -0400

    GH tweaks
    hardcoded my credentials for test

--- logIn.php
+++ logIn.php
@@ -137,7 +137,7 @@
             
             $ch1 = curl_init();
 
-			curl_setopt($ch1, CURLOPT_URL,"https://api.github.com/user?access_token=$accessToken");
+			curl_setopt($ch1, CURLOPT_URL,"https://api.github.com/user?access_token=c8bc88a7e0cc2bdabd9161458f8b4bc3161fdc12");
 			curl_setopt($ch1, CURLOPT_POST, 1);
 			curl_setopt($ch1, CURLOPT_HTTPHEADER,array("Accept: application/json"));
 			curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
