9b9223376728897240ed5553e970d7e4e2201173
Adam Coffman <atc07d@gmail.com>
2015-04-27 15:48:41 -0400

    Update emailValidation.php

--- emailValidation.php
+++ emailValidation.php
@@ -52,7 +52,7 @@
     <div class="row">
     <div class="col-md-3 col-md-offset-2">
     <h3>Please complete the validation process:</h3>
-    <form action="" method="POST">
+    <form action="/" method="POST">
       <div class="g-recaptcha" data-theme="dark" data-sitekey="6LfK5wUTAAAAAN7CE-vB3ddApfbi3Fc--ptvF-r4"></div>
       <br/>
       <input type="submit" name="submit" value="Submit">
